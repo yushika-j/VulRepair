3f90ed2da0bd47d98108955adf63539e86dcc0a9
rinaxosman <94803092+rinaxosman@users.noreply.github.com>
2025-03-20 12:04:07 -0400

    model 4 paths updated

--- M4_T5_base_NL/t5_base_main.py
+++ M4_T5_base_NL/t5_base_main.py
@@ -62,12 +62,16 @@ class TextDataset(Dataset):
         elif file_type == "eval":
             sources = val_data["source"].tolist()
             labels = val_data["target"].tolist()
-        elif file_type == "test":
+        # elif file_type == "test":
             #data = datasets.load_dataset("MickyMike/cvefixes_bigvul", split="test")
-            data = datasets.load_dataset("/lustre04/scratch/rinao/VulRepair/cvefixes_bigvul/test.csv") 
+        #     data = datasets.load_dataset("/lustre04/scratch/rinao/VulRepair/cvefixes_bigvul/test.csv") 
 
-            sources = data["source"]
-            labels = data["target"]
+        #     sources = data["source"]
+        #     labels = data["target"]
+        # elif file_type == "test":
+            test_df = pd.read_csv("../data/cvefixes_bigvul/test.csv")
+            sources = test_df["source"].tolist()
+            labels = test_df["target"].tolist()
         self.examples = []
         for i in tqdm(range(len(sources))):
             self.examples.append(convert_examples_to_features(sources[i], labels[i], tokenizer, args))
@@ -388,7 +392,8 @@ def main():
     # Training
     if args.do_train:
         #train_data_whole = datasets.load_dataset("MickyMike/cvefixes_bigvul", split="train")
-        train_df = pd.read_csv("/lustre04/scratch/rinao/VulRepair/cvefixes_bigvul/train.csv")
+        # train_df = pd.read_csv("/lustre04/scratch/rinao/VulRepair/cvefixes_bigvul/train.csv")
+        train_df = pd.read_csv("../data/cvefixes_bigvul/train.csv")
         train_data_whole = Dataset.from_pandas(train_df)
                 
         df = pd.DataFrame({"source": train_data_whole["source"], "target": train_data_whole["target"]})
